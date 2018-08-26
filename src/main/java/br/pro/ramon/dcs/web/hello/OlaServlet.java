7d7e2809aaf58b23df47503c5dae61da4bd657d9
Prof. Ramon <ramon@ramon.pro.br>
2018-08-26 16:14:43 -0300

    Melhoria nos nomes dos parametros: req e resp -> request e response.

--- src/main/java/br/pro/ramon/dcs/web/hello/OlaServlet.java
+++ src/main/java/br/pro/ramon/dcs/web/hello/OlaServlet.java
@@ -12,28 +12,28 @@ import javax.servlet.http.HttpServletResponse;
 public class OlaServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String nome = req.getParameter("nome");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nome = request.getParameter("nome");
 
         if (nome == null || nome.trim().isEmpty()) {
             nome = "mundo";
         }
 
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
         out.println("Olá, <strong>" + nome + "</strong>! (via GET)");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String nome = req.getParameter("nome");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nome = request.getParameter("nome");
 
         if (nome == null || nome.trim().isEmpty()) {
             nome = "mundo";
         }
 
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
         out.println("Olá, <strong>" + nome + "</strong>! (via POST)");
     }
 
